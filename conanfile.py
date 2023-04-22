35ee88c53aa26413e1574ac000df899225f009f9
Ulrich Eck <ulrich.eck@tum.de>
2023-04-22 11:23:52 +0200

    conan 2.0 drafts for camp_common and cuda_dev_config small fixes for corrade

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env cuda
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conan import ConanFile
 import os
 
 # pylint: disable=W0201
 class CUDADevConfigConan(ConanFile):
-    python_requires = "camp_common/[>=0.1]@camposs/stable"
+    python_requires = "camp_common/0.5@camposs/stable"
     python_requires_extend = "camp_common.CampCudaBase"
 
     name = "cuda_dev_config"
-    version = "2.0"
+    version = "2.1"
     license = "Proprietary Dependency"
     exports = ["LICENSE.md"]
     description = "Configuration of CUDA SDK for use as a development dependency."
-    url = "https://github.com/ulricheck/conan-cuda_dev_config"
+    url = "https://github.com/TUM-CONAN/conan-cuda_dev_config"
     author = "Ulrich Eck <ulrich.eck@tum.de>"
     options = { 
-        "cuda_version": ["11.7","11.6","11.5","11.4","11.2","11.1","11.0","10.2", "10.1", "10.0", "9.1", "9.0"],
-        "cuda_root": "ANY",
+        "cuda_version": ["12.1","12.0","11.8","11.7","11.6","11.5","11.4","11.2","11.1","11.0","10.2", "10.1", "10.0"],
+        "cuda_root": ["ANY", ],
         }
     default_options = {
-        "cuda_version": "11.4", 
+        "cuda_version": "11.7",
         "cuda_root": "ANY",
     }
 
@@ -31,14 +31,13 @@ class CUDADevConfigConan(ConanFile):
     def validate(self):
         assert(self.have_cuda_dev)
 
-
     def package_info(self):
         if self.have_cuda_dev:
-            self.cpp_info.bindirs = [self._cuda_bin_dir,]
-            self.user_info.cuda_version = self._cuda_version
-            self.user_info.cuda_root = self._cuda_sdk_root
-            self.env_info.path.append(self._cuda_sdk_root)
-            self.env_info.CUDA_SDK_ROOT_DIR = self._cuda_sdk_root
+            self.cpp_info.bindirs.append(self._cuda_bin_dir)
+            self.conf_info.define("cuda_dev_config:cuda_version", self._cuda_version)
+            self.conf_info.define("cuda_dev_config:cuda_root", self._cuda_sdk_root)
+            self.buildenv_info.append_path("PATH", self._cuda_sdk_root)
+            self.buildenv_info.define_path("CUDA_SDK_ROOT_DIR", self._cuda_sdk_root)
 
     @property
     def have_cuda_dev(self):
